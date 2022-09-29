e948e75450c690557e622f29af292eab03a32417
Chinnayya Naidu Nalla <chinnayya.nalla@dtworld.ae>
2022-09-29 22:46:07 +0400

    Styles

--- home-ui/src/app/app-routing.module.ts
+++ home-ui/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -6,18 +5,6 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./modules/layout/secure-layout/secure-layout.module').then((module) => module.SecureLayoutModule)
-  },
-  // Way 2 => Loading by Providing URL in Routing
-  {
-    path: 'contacts',
-    loadChildren: () =>
-      loadRemoteModule({
-        remoteName: 'contacts',
-        remoteEntry: 'http://localhost:4200/remoteEntry.js',
-        exposedModule: './ContactUsModule'
-      }).then((module) => {
-        return module.ContactUsModule;
-      })
   }
 ];
 
--- home-ui/src/app/modules/layout/secure-layout/secure-layout-routing.module.ts
+++ home-ui/src/app/modules/layout/secure-layout/secure-layout-routing.module.ts
@@ -1,3 +1,4 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from '../../feature/common/not-found/not-found.component';
@@ -12,6 +13,18 @@ const routes: Routes = [
         // Way 1 => Loading using Declarations
         path: 'locations',
         loadChildren: () => import('locations/LocationsModule').then((module) => module.LocationsModule) 
+      },
+      {
+        // Way 2 => Loading by Providing URL in Routing
+        path: 'contacts',
+        loadChildren: () =>
+          loadRemoteModule({
+            remoteName: 'contacts',
+            remoteEntry: 'http://localhost:4200/remoteEntry.js',
+            exposedModule: './ContactUsModule'
+          }).then((module) => {
+            return module.ContactUsModule;
+          })
       },
       {
         path: '**',
