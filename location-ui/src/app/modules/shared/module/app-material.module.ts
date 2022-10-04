9fc8f709d653bda184d8827efe3e1521bb0316cc
Chinnayya Naidu Nalla <chinnayya.nalla@dtworld.ae>
2022-10-04 14:22:33 +0400

    Shared and App Material

--- location-ui/src/app/modules/shared/module/app-material.module.ts
+++ location-ui/src/app/modules/shared/module/app-material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [],
-  imports: []
+  imports: [
+    CommonModule
+  ]
 })
 export class AppMaterialModule { }
