b618d4e71eab9b48fb2a0be6a194935453a4fd0c
Chinnayya Naidu Nalla <chinnayya.nalla@dtworld.ae>
2022-10-04 22:06:57 +0400

    Changed Bootstrap Version

--- location-ui/src/app/modules/shared/module/app-material.module.ts
+++ location-ui/src/app/modules/shared/module/app-material.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
-
-
-
 @NgModule({
   declarations: [],
   imports: [
