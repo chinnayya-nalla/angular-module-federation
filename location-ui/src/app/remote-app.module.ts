e075906a1e8a2465730039adde0853b90fe6714e
Chinnayya Naidu Nalla <chinnayya.nalla@dtworld.ae>
2022-10-05 20:24:33 +0400

    Ordering Styles

--- location-ui/src/app/remote-app.module.ts
+++ location-ui/src/app/remote-app.module.ts
@@ -4,13 +4,15 @@ import { SharedModule } from './modules/shared/module/shared.module';
 import { SecureModule } from './modules/layout/secure/secure.module';
 
 import { MatSidenavService } from './modules/shared/service/mat-sidenav.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     SharedModule,
-    SecureModule
+    SecureModule,
+    ReactiveFormsModule
   ],
   providers: [
     MatSidenavService
